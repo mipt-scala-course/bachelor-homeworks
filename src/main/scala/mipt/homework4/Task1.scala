d7aed3e00037aa89e7bd66cad71b56d069210ee9
Anton Brovkin <a.a.brovkin@tinkoff.ru>
2023-03-09 11:01:45 +0300

    Fixed lint

--- src/main/scala/mipt/homework4/Task1.scala
+++ src/main/scala/mipt/homework4/Task1.scala
@@ -7,8 +7,8 @@ import scala.annotation.tailrec
 object Task1 {
 
   /**
-   * MyStream - ленивый список, аналогичный LazyList в Scala
-   */
+    * MyStream - ленивый список, аналогичный LazyList в Scala
+    */
   sealed trait MyStream[+A] {
     import MyStream._
 
@@ -17,22 +17,22 @@ object Task1 {
     def filter(predicate: A => Boolean): MyStream[A] =
       task"""
             Реализуйте метод фильтрации ленивого списка. Результатом тоже должен быть ленивый список
-          """(1, 1)
+          """ (1, 1)
 
     def dropWhile(predicate: A => Boolean): MyStream[A] =
       task"""
             Реализуйте метод, который лениво отбросит все элементы, пока выполняется условие на элемент
-          """(1, 2)
+          """ (1, 2)
 
     def flatMap[B](f: A => MyStream[B]): MyStream[B] =
       task"""
              Реализуйте метод flatMap, аналогичный такому у списка
              Метод должен работать лениво
-          """(1, 3)
+          """ (1, 3)
   }
 
   case class NonEmptyStream[A] private (h: () => A, t: () => MyStream[A]) extends MyStream[A]
-  case object Empty extends MyStream[Nothing]
+  case object Empty                                                       extends MyStream[Nothing]
 
   object MyStream {
     def apply[A](h: => A, t: => MyStream[A]): MyStream[A] =
@@ -42,7 +42,7 @@ object Task1 {
     private def takeInner[A](stream: MyStream[A], n: Int, acc: List[A]): List[A] =
       stream match {
         case NonEmptyStream(h, t) if n > 0 => takeInner(t(), n - 1, h() :: acc)
-        case _ => acc
+        case _                             => acc
       }
   }
 }
