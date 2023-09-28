c8171a9660afdd89df99e7e5cc3a8e5415281cbf
Anton Brovkin <a.a.brovkin@tinkoff.ru>
2023-09-28 10:52:25 +0300

    Removed task

--- src/main/scala/mipt/functors/OtherInstances.scala
+++ src/main/scala/mipt/functors/OtherInstances.scala
@@ -8,14 +8,3 @@ trait OtherInstances:
   /** Реализуйте инстанс Functor для Arr[C, *] (по второму аргументу)
     */
   given [C]: Functor[Arr[C, *]] = ???
-
-  /** Реализуйте инстанс для композиции функторов и докажите законы функтора для этого инстанса
-    */
-  given [F[_]: Functor, G[_]: Functor]: Functor[[x] =>> F[G[x]]] =
-    new Functor[[x] =>> F[G[x]]]:
-      override def map[A, B](fga: F[G[A]])(f: A => B): F[G[B]] =
-        val fFunctor = summon[Functor[F]]
-        val gFunctor = summon[Functor[G]]
-        fFunctor.map(fga) { ga =>
-          gFunctor.map(ga)(f)
-        }
